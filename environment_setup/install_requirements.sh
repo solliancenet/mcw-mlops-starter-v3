c14eb878ddfba8e277aac4763a4acdd0b5fbf5c9
Jitendra Shirolkar <iit88@yahoo.com>
2021-08-24 10:46:02 -0400

    Update install_requirements.sh

--- environment_setup/install_requirements.sh
+++ environment_setup/install_requirements.sh
@@ -1,4 +1,4 @@
 python --version
-pip install azure-cli==2.14.0
-pip install azureml-sdk==1.17.0
+pip install azure-cli==2.27.2
+pip install azureml-sdk==1.33.0
 pip install azureml-dataprep[pandas]
